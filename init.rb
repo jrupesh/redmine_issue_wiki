4d266718b4a1f59cafe0ceae16bc5d25cf7f8823
jrupesh <j.rupesh@gmail.com>
2014-08-11 10:44:41 +0530

    Initial commit.

--- init.rb
+++ init.rb
@@ -1,10 +1,10 @@
 require 'redmine'
-require 'redmine_issue_history_listener.rb'
+require 'redmine_issue_wiki_listener.rb'
 
-Redmine::Plugin.register :redmine_issue_history do
-  name 'redmine_issue_history'
-  author 'Systango'
-  description 'This plugin provide history of issue in tabs representation'
+Redmine::Plugin.register :redmine_issue_wiki do
+  name 'Redmine Issue Wiki'
+  author 'Rupesh J'
+  description 'This plugin provide wiki as a tab representation in a Issue.'
   version '1.0.0'
-  requires_redmine :version_or_higher => '2.2.4'
+  requires_redmine :version_or_higher => '2.5'
 end
